cc0e72c178affee2d431c4eafd864c68d36c8aae
ricky-bruner <alteredecho07@yahoo.com>
2018-08-01 14:29:07 -0500

    built attractions function and DOM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 "use strict";
 
 // This is here to get you started. You may remove or move as necessary.
+let builtDOM = require('./buildDOM.js');
 let parkInfo = require('./parkInfo.js');
+let attractions = require('./attractions.js');
+
+builtDOM();
